2402ca1daf703605d6543bf9a789860b2bb64bc5
Marco Zanin <mrczn.bb@gmail.com>
2021-02-12 22:08:57 +0100

    Add a basic videoplayer for radio 105 TV
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- app/src/main/java/com/bb/the105zoo/ui/tv/TvFragment.java
+++ app/src/main/java/com/bb/the105zoo/ui/tv/TvFragment.java
@@ -1,10 +1,13 @@
 package com.bb.the105zoo.ui.tv;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.MediaController;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -21,8 +24,17 @@ public class TvFragment extends Fragment {
         tvViewModel =
                 new ViewModelProvider(this).get(TvViewModel.class);
         View root = inflater.inflate(R.layout.fragment_tv, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        tvViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+
+        String videoUrl = "https://live2t-radio-mediaset-it.akamaized.net/content/hls_h0_clr_vos/live/channel(ec)/index.m3u8?hdnts=st=1613162433~exp=1613176863~acl=/content/hls_h0_clr_vos/live/channel(ec)*~hmac=aaffca0db29392483c0720e80adbf3f1d30e773e1068063265fa81970cfb982e";
+        VideoView videoView = root.findViewById(R.id.videoView);
+//Use a media controller so that you can scroll the video contents
+//and also to pause, start the video.
+        MediaController mediaController = new MediaController(getContext());
+        mediaController.setAnchorView(videoView);
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(Uri.parse(videoUrl));
+        videoView.start();
+
         return root;
     }
 }

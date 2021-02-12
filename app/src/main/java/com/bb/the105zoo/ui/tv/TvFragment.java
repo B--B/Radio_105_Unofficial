3a38f9b6a02ab8c16e8d11e7fb50f66ffd28b352
Marco Zanin <mrczn.bb@gmail.com>
2021-02-12 23:24:37 +0100

    Add progress bar for video player
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- app/src/main/java/com/bb/the105zoo/ui/tv/TvFragment.java
+++ app/src/main/java/com/bb/the105zoo/ui/tv/TvFragment.java
@@ -1,11 +1,13 @@
 package com.bb.the105zoo.ui.tv;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.MediaController;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -18,6 +20,10 @@ import com.bb.the105zoo.R;
 public class TvFragment extends Fragment {
 
     private TvViewModel tvViewModel;
+    private ProgressBar progressBar;
+    private int mCurrentPosition = 0;
+    VideoView videoView;
+    String videoUrl;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -25,16 +31,36 @@ public class TvFragment extends Fragment {
                 new ViewModelProvider(this).get(TvViewModel.class);
         View root = inflater.inflate(R.layout.fragment_tv, container, false);
 
-        String videoUrl = "https://live2t-radio-mediaset-it.akamaized.net/content/hls_h0_clr_vos/live/channel(ec)/index.m3u8?hdnts=st=1613162433~exp=1613176863~acl=/content/hls_h0_clr_vos/live/channel(ec)*~hmac=aaffca0db29392483c0720e80adbf3f1d30e773e1068063265fa81970cfb982e";
-        VideoView videoView = root.findViewById(R.id.videoView);
-//Use a media controller so that you can scroll the video contents
-//and also to pause, start the video.
+        videoView = root.findViewById(R.id.videoView);
+        progressBar = root.findViewById(R.id.progressBar);
+        videoUrl = "https://live2t-radio-mediaset-it.akamaized.net/content/hls_h0_clr_vos/live/channel(ec)/index.m3u8?hdnts=st=1613162433~exp=1613176863~acl=/content/hls_h0_clr_vos/live/channel(ec)*~hmac=aaffca0db29392483c0720e80adbf3f1d30e773e1068063265fa81970cfb982e";
+
+        progressBar.setVisibility(View.VISIBLE);
         MediaController mediaController = new MediaController(getContext());
         mediaController.setAnchorView(videoView);
         videoView.setMediaController(mediaController);
         videoView.setVideoURI(Uri.parse(videoUrl));
         videoView.start();
 
+        videoView.setOnInfoListener(onInfoToPlayStateListener);
+
         return root;
     }
+
+    private final MediaPlayer.OnInfoListener onInfoToPlayStateListener = new MediaPlayer.OnInfoListener() {
+
+        @Override
+        public boolean onInfo(MediaPlayer mp, int what, int extra) {
+            if (MediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START == what) {
+                progressBar.setVisibility(View.GONE);
+            }
+            if (MediaPlayer.MEDIA_INFO_BUFFERING_START == what) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+            if (MediaPlayer.MEDIA_INFO_BUFFERING_END == what) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+            return false;
+        }
+    };
 }

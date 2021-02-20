ec7597dd827aea8ef353001e5a6d3a96ef65921f
Marco Zanin <mrczn.bb@gmail.com>
2021-02-20 23:24:36 +0100

    Provide a Callback for back button pression
    Also tune a bit the code
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- app/src/main/java/com/bb/radio105/PodcastFragment.java
+++ app/src/main/java/com/bb/radio105/PodcastFragment.java
@@ -8,23 +8,65 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import org.jetbrains.annotations.NotNull;
+
 public class PodcastFragment extends Fragment {
+
+    WebView mWebView = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_podcast, container, false);
 
-        WebView mWebView = root.findViewById(R.id.webView);
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                if (mWebView.canGoBack()) {
+                    mWebView.goBack();
+                }
+            }
+        };
+        requireActivity().getOnBackPressedDispatcher().addCallback(getViewLifecycleOwner(), callback);
 
+        setRetainInstance(true);
+        mWebView = root.findViewById(R.id.webView);
         String url = "https://www.105.net/sezioni/995/podcast";
+        final String javaScript = "javascript:(function() { " +
+                "var element = document.getElementsByClassName('navbar-fixed-top hidden-print');" +
+                " if (element.length) { element[0].style.display = 'none' }; " +
+                "var element = document.getElementsByClassName('container vc_bg_white');" +
+                " if (element.length) { element[0].style.display = 'none' }; " +
+                "var element = document.getElementsByClassName('col-xs-12 vc_p0');" +
+                " if (element.length) { element[0].style.display = 'none' }; " +
+                "var element = document.getElementsByClassName('container-fluid vc_bg_darkgray vc_bt7_yellow vc_z2 vc_hidden_print');" +
+                " if (element.length) { element[0].style.display = 'none' }; " +
+                "var element = document.getElementsByClassName('container-fluid vc_bg_color_program');" +
+                " if (element.length) { element[0].style.display = 'none' }; " +
+                "var element = document.getElementsByClassName('container-fluid vc_bg_white vc_pl_pr_0_mobile');" +
+                " if (element.length) { element[0].style.display = 'none' }; " +
+                "var element = document.getElementsByClassName('vc_search_refine_results_standard');" +
+                " if (element.length) { element[0].style.display = 'none' }; " +
+                "var element = document.getElementsByClassName('text_edit vc_textedit_title_refine_results null');" +
+                " if (element.length) { element[0].style.display = 'none' }; " +
+                "var element = document.getElementsByClassName('anteprima_articolo article_cont vc_preview_medium_bt_podcast vc_txt_m variant vc_theme_light vc_br_60 null Zoo di 105 vc_section_lo-zoo-di-105-home vc_macro_section_canale-lo-zoo-di-105 homezoo scheda cms_article ');" +
+                " if (element.length) { element[0].style.display = 'none' }; " +
+                "var element = document.getElementsByClassName('social social_buttons');" +
+                " if (element.length) { element[0].style.display = 'none' }; " + "})()";
 
         mWebView.getSettings().setLoadsImagesAutomatically(true);
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        mWebView.loadUrl(url);
+
+        if (savedInstanceState != null) {
+            // Restore last state for checked position.
+            mWebView.restoreState(savedInstanceState);
+        } else  {
+            mWebView.loadUrl(url);
+        }
 
         mWebView.setWebViewClient(new WebViewClient() {
 
@@ -43,32 +85,24 @@ public class PodcastFragment extends Fragment {
 
             @Override
             public void onPageFinished (WebView mWebView, String url) {
-                mWebView.loadUrl("javascript:(function() { " +
-                        "var element = document.getElementsByClassName('navbar-fixed-top hidden-print');" +
-                        " if (element.length) { element[0].style.display = 'none' }; " +
-                        "var element = document.getElementsByClassName('container vc_bg_white');" +
-                        " if (element.length) { element[0].style.display = 'none' }; " +
-                        "var element = document.getElementsByClassName('col-xs-12 vc_p0');" +
-                        " if (element.length) { element[0].style.display = 'none' }; " +
-                        "var element = document.getElementsByClassName('container-fluid vc_bg_darkgray vc_bt7_yellow vc_z2 vc_hidden_print');" +
-                        " if (element.length) { element[0].style.display = 'none' }; " +
-                        "var element = document.getElementsByClassName('container-fluid vc_bg_color_program');" +
-                        " if (element.length) { element[0].style.display = 'none' }; " +
-                        "var element = document.getElementsByClassName('container-fluid vc_bg_white vc_pl_pr_0_mobile');" +
-                        " if (element.length) { element[0].style.display = 'none' }; " +
-                        "var element = document.getElementsByClassName('vc_search_refine_results_standard');" +
-                        " if (element.length) { element[0].style.display = 'none' }; " +
-                        "var element = document.getElementsByClassName('text_edit vc_textedit_title_refine_results null');" +
-                        " if (element.length) { element[0].style.display = 'none' }; " +
-                        "var element = document.getElementsByClassName('anteprima_articolo article_cont vc_preview_medium_bt_podcast vc_txt_m variant vc_theme_light vc_br_60 null Zoo di 105 vc_section_lo-zoo-di-105-home vc_macro_section_canale-lo-zoo-di-105 homezoo scheda cms_article ');" +
-                        " if (element.length) { element[0].style.display = 'none' }; " +
-                        "var element = document.getElementsByClassName('social social_buttons');" +
-                        " if (element.length) { element[0].style.display = 'none' }; " + "})()");
+                mWebView.loadUrl(javaScript);
                 root.findViewById(R.id.loading).setVisibility(View.GONE);
                 mWebView.setVisibility(View.VISIBLE);
                 super.onPageFinished(mWebView, url);
             }
         });
         return root;
+    }
+
+    @Override
+    public void onSaveInstanceState(@NotNull Bundle savedInstanceState) {
+        mWebView.saveState(savedInstanceState);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        mWebView.destroy();
+        super.onDestroy();
     }
 }

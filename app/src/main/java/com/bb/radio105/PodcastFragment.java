c1f5e7046fe4378c48717056375018dc32f264dd
Marco Zanin <mrczn.bb@gmail.com>
2021-02-20 02:58:58 +0100

    Finish podcast fragment
    Remove The 105 Zoo on podcast section, i will add a separate section
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- app/src/main/java/com/bb/radio105/PodcastFragment.java
+++ app/src/main/java/com/bb/radio105/PodcastFragment.java
@@ -1,5 +1,6 @@
 package com.bb.radio105;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +18,6 @@ public class PodcastFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_podcast, container, false);
 
         WebView mWebView = root.findViewById(R.id.webView);
-        mWebView.setWebViewClient(new PodcastBrowser());
 
         String url = "https://www.105.net/sezioni/995/podcast";
 
@@ -25,38 +25,50 @@ public class PodcastFragment extends Fragment {
         mWebView.getSettings().setJavaScriptEnabled(true);
         mWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         mWebView.loadUrl(url);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView mWebView, String url, Bitmap mBitmap) {
+                mWebView.setVisibility(View.GONE);
+                root.findViewById(R.id.loading).setVisibility(View.VISIBLE);
+                super.onPageStarted(mWebView, url, mBitmap);
+            }
+
+            @Override
+            public void onPageFinished (WebView mWebView, String url) {
+                mWebView.loadUrl("javascript:(function() { " +
+                        "var element = document.getElementsByClassName('navbar-fixed-top hidden-print');" +
+                        " if (element.length) { element[0].style.display = 'none' }; " +
+                        "var element = document.getElementsByClassName('container vc_bg_white');" +
+                        " if (element.length) { element[0].style.display = 'none' }; " +
+                        "var element = document.getElementsByClassName('col-xs-12 vc_p0');" +
+                        " if (element.length) { element[0].style.display = 'none' }; " +
+                        "var element = document.getElementsByClassName('container-fluid vc_bg_darkgray vc_bt7_yellow vc_z2 vc_hidden_print');" +
+                        " if (element.length) { element[0].style.display = 'none' }; " +
+                        "var element = document.getElementsByClassName('container-fluid vc_bg_color_program');" +
+                        " if (element.length) { element[0].style.display = 'none' }; " +
+                        "var element = document.getElementsByClassName('container-fluid vc_bg_white vc_pl_pr_0_mobile');" +
+                        " if (element.length) { element[0].style.display = 'none' }; " +
+                        "var element = document.getElementsByClassName('vc_search_refine_results_standard');" +
+                        " if (element.length) { element[0].style.display = 'none' }; " +
+                        "var element = document.getElementsByClassName('text_edit vc_textedit_title_refine_results null');" +
+                        " if (element.length) { element[0].style.display = 'none' }; " +
+                        "var element = document.getElementsByClassName('anteprima_articolo article_cont vc_preview_medium_bt_podcast vc_txt_m variant vc_theme_light vc_br_60 null Zoo di 105 vc_section_lo-zoo-di-105-home vc_macro_section_canale-lo-zoo-di-105 homezoo scheda cms_article ');" +
+                        " if (element.length) { element[0].style.display = 'none' }; " +
+                        "var element = document.getElementsByClassName('social social_buttons');" +
+                        " if (element.length) { element[0].style.display = 'none' }; " + "})()");
+                root.findViewById(R.id.loading).setVisibility(View.GONE);
+                mWebView.setVisibility(View.VISIBLE);
+                super.onPageFinished(mWebView, url);
+            }
+        });
         return root;
-    }
-
-    private static class PodcastBrowser extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-
-        @Override
-        public void onPageFinished (WebView mWebView, String url) {
-            mWebView.loadUrl("javascript:(function() { " +
-                    "var element = document.getElementsByClassName('navbar-fixed-top hidden-print');" +
-                    " if (element.length) { element[0].style.display = 'none' }; " +
-                    "var element = document.getElementsByClassName('container vc_bg_white');" +
-                    " if (element.length) { element[0].style.display = 'none' }; " +
-                    "var element = document.getElementsByClassName('col-xs-12 vc_p0');" +
-                    " if (element.length) { element[0].style.display = 'none' }; " +
-                    "var element = document.getElementsByClassName('container-fluid vc_bg_darkgray vc_bt7_yellow vc_z2 vc_hidden_print');" +
-                    " if (element.length) { element[0].style.display = 'none' }; " +
-                    "var element = document.getElementsByClassName('container-fluid vc_bg_color_program');" +
-                    " if (element.length) { element[0].style.display = 'none' }; " +
-                    "var element = document.getElementsByClassName('container-fluid vc_bg_white vc_pl_pr_0_mobile');" +
-                    " if (element.length) { element[0].style.display = 'none' }; " +
-                    "var element = document.getElementsByClassName('vc_search_refine_results_standard');" +
-                    " if (element.length) { element[0].style.display = 'none' }; " +
-                    "var element = document.getElementsByClassName('text_edit vc_textedit_title_refine_results null');" +
-                    " if (element.length) { element[0].style.display = 'none' }; " +
-                    "var element = document.getElementsByClassName('social social_buttons');" +
-                    " if (element.length) { element[0].style.display = 'none' }; " + "})()");
-            super.onPageFinished(mWebView, url);
-        }
     }
 }
--- app/src/main/java/com/bb/radio105/Utils.java
+++ app/src/main/java/com/bb/radio105/Utils.java
@@ -2,12 +2,6 @@ package com.bb.radio105;
 
 import android.app.ActivityManager;
 import android.content.Context;
-import android.util.Log;
-import android.util.Patterns;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Matcher;
 
 public class Utils {
     static boolean isRadioStreamingRunning(Context context) {
@@ -28,16 +22,5 @@ public class Utils {
             }
         }
         return false;
-    }
-
-    static String[] extractLinks(String text) {
-        List<String> links = new ArrayList<String>();
-        Matcher m = Patterns.WEB_URL.matcher(text);
-        while (m.find()) {
-            String url = m.group();
-            links.add(url);
-        }
-
-        return links.toArray(new String[links.size()]);
     }
 }

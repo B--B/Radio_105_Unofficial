615d768c20e81ecc2d7f7f0693608273d2dc9b91
Marco Zanin <mrczn.bb@gmail.com>
2021-03-05 22:15:48 +0100

    ZooColors: move all related code from Utils to main activity
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- app/src/main/java/com/bb/radio105/MainActivity.java
+++ app/src/main/java/com/bb/radio105/MainActivity.java
@@ -1,10 +1,16 @@
 package com.bb.radio105;
 
+import android.content.res.ColorStateList;
+import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
@@ -68,9 +74,153 @@ public class MainActivity extends AppCompatActivity implements  UpdateColorsInte
     @Override
     public void onUpdate(boolean zooColors) {
         if (zooColors) {
-            Utils.setZooColors(this);
+            setZooColors();
         } else {
-            Utils.setStockColors(this);
+            setStockColors();
+        }
+    }
+
+    void setZooLightColors() {
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.zoo_300));
+        Objects.requireNonNull(((AppCompatActivity) this).getSupportActionBar())
+                .setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.zoo_500)));
+        NavigationView mNavigationView = findViewById(R.id.nav_view);
+        View header = mNavigationView.getHeaderView(0);
+        LinearLayout mLinearLayout = header.findViewById(R.id.nav_header);
+        mLinearLayout.setBackgroundResource(R.drawable.side_nav_bar_zoo);
+        ColorStateList mColorStateList = new ColorStateList(
+                new int[][] {
+                        new int[] {-android.R.attr.state_checked},
+                        new int[] { android.R.attr.state_checked}
+                },
+                new int[] {
+                        ContextCompat.getColor(this, R.color.black),
+                        ContextCompat.getColor(this, R.color.zoo_500),
+                }
+        );
+        mNavigationView.setItemIconTintList(mColorStateList);
+        mNavigationView.setItemTextColor(mColorStateList);
+    }
+
+    void setZooDarkColors() {
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.zoo_200));
+        Objects.requireNonNull(((AppCompatActivity) this).getSupportActionBar())
+                .setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.zoo_500)));
+        NavigationView mNavigationView = findViewById(R.id.nav_view);
+        View header = mNavigationView.getHeaderView(0);
+        LinearLayout mLinearLayout = header.findViewById(R.id.nav_header);
+        mLinearLayout.setBackgroundResource(R.drawable.side_nav_bar_zoo);
+        ColorStateList mColorStateList = new ColorStateList(
+                new int[][] {
+                        new int[] {-android.R.attr.state_checked},
+                        new int[] { android.R.attr.state_checked}
+                },
+                new int[] {
+                        ContextCompat.getColor(this, R.color.white),
+                        ContextCompat.getColor(this, R.color.zoo_500),
+                }
+        );
+        mNavigationView.setItemIconTintList(mColorStateList);
+        mNavigationView.setItemTextColor(mColorStateList);
+    }
+
+    void setStockLightColors() {
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.orange_900));
+        Objects.requireNonNull(((AppCompatActivity) this).getSupportActionBar())
+                .setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.yellow_700)));
+        NavigationView mNavigationView = findViewById(R.id.nav_view);
+        View header = mNavigationView.getHeaderView(0);
+        LinearLayout mLinearLayout = header.findViewById(R.id.nav_header);
+        mLinearLayout.setBackgroundResource(R.drawable.side_nav_bar);
+        ColorStateList mColorStateList = new ColorStateList(
+                new int[][] {
+                        new int[] {-android.R.attr.state_checked},
+                        new int[] { android.R.attr.state_checked}
+                },
+                new int[] {
+                        ContextCompat.getColor(this, R.color.black),
+                        ContextCompat.getColor(this, R.color.yellow_700),
+                }
+        );
+        mNavigationView.setItemIconTintList(mColorStateList);
+        mNavigationView.setItemTextColor(mColorStateList);
+    }
+
+    void setStockDarkColors() {
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.yellow_700));
+        Objects.requireNonNull(((AppCompatActivity) this).getSupportActionBar())
+                .setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(this, R.color.yellow_200)));
+        NavigationView mNavigationView = findViewById(R.id.nav_view);
+        View header = mNavigationView.getHeaderView(0);
+        LinearLayout mLinearLayout = header.findViewById(R.id.nav_header);
+        mLinearLayout.setBackgroundResource(R.drawable.side_nav_bar);
+        ColorStateList mColorStateList = new ColorStateList(
+                new int[][] {
+                        new int[] {-android.R.attr.state_checked},
+                        new int[] { android.R.attr.state_checked}
+                },
+                new int[] {
+                        ContextCompat.getColor(this, R.color.white),
+                        ContextCompat.getColor(this, R.color.yellow_200)
+                }
+        );
+        mNavigationView.setItemIconTintList(mColorStateList);
+        mNavigationView.setItemTextColor(mColorStateList);
+    }
+
+    void setZooColors() {
+        final String[] darkModeValues = getResources().getStringArray(R.array.theme_values);
+        // The apps theme is decided depending upon the saved preferences on app startup
+        String themePref = PreferenceManager.getDefaultSharedPreferences(this)
+                .getString(getString(R.string.theme_key), getString(R.string.theme_default_value));
+        // Comparing to see which preference is selected and applying those theme settings
+        if (themePref.equals(darkModeValues[0])) {
+            // Check system status and apply colors
+            int nightModeOn = getResources().getConfiguration().uiMode &
+                    Configuration.UI_MODE_NIGHT_MASK;
+            switch (nightModeOn) {
+                case Configuration.UI_MODE_NIGHT_YES:
+                    setZooDarkColors();
+                    break;
+                case Configuration.UI_MODE_NIGHT_NO:
+                case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                    setZooLightColors();
+                    break;
+            }
+        }
+        if (themePref.equals(darkModeValues[1])) {
+            setZooLightColors();
+        }
+        if (themePref.equals(darkModeValues[2])) {
+            setZooDarkColors();
+        }
+    }
+
+    void setStockColors() {
+        final String[] darkModeValues = getResources().getStringArray(R.array.theme_values);
+        // The apps theme is decided depending upon the saved preferences on app startup
+        String themePref = PreferenceManager.getDefaultSharedPreferences(this)
+                .getString(getString(R.string.theme_key), getString(R.string.theme_default_value));
+        // Comparing to see which preference is selected and applying those theme settings
+        if (themePref.equals(darkModeValues[0])) {
+            // Check system status and apply colors
+            int nightModeOn = getResources().getConfiguration().uiMode &
+                    Configuration.UI_MODE_NIGHT_MASK;
+            switch (nightModeOn) {
+                case Configuration.UI_MODE_NIGHT_YES:
+                    setStockDarkColors();
+                    break;
+                case Configuration.UI_MODE_NIGHT_NO:
+                case Configuration.UI_MODE_NIGHT_UNDEFINED:
+                    setStockLightColors();
+                    break;
+            }
+        }
+        if (themePref.equals(darkModeValues[1])) {
+            setStockLightColors();
+        }
+        if (themePref.equals(darkModeValues[2])) {
+            setStockDarkColors();
         }
     }
 }
--- app/src/main/java/com/bb/radio105/Utils.java
+++ app/src/main/java/com/bb/radio105/Utils.java
@@ -4,23 +4,16 @@ import android.Manifest;
 import android.app.Activity;
 import android.app.DownloadManager;
 import android.content.Context;
-import android.content.res.ColorStateList;
-import android.content.res.Configuration;
-import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
 import android.view.View;
 import android.view.WindowInsets;
 import android.view.WindowInsetsController;
-import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.preference.PreferenceManager;
 
-import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Objects;
@@ -86,149 +79,5 @@ public class Utils {
             mActivity.getWindow().clearFlags(FLAG_FULLSCREEN);
         }
         Objects.requireNonNull(((AppCompatActivity) mActivity).getSupportActionBar()).show();
-    }
-
-    static void setZooLightColors(Activity mActivity) {
-        mActivity.getWindow().setStatusBarColor(ContextCompat.getColor(mActivity, R.color.zoo_300));
-        Objects.requireNonNull(((AppCompatActivity) mActivity).getSupportActionBar())
-                .setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(mActivity, R.color.zoo_500)));
-        NavigationView mNavigationView = mActivity.findViewById(R.id.nav_view);
-        View header = mNavigationView.getHeaderView(0);
-        LinearLayout mLinearLayout = header.findViewById(R.id.nav_header);
-        mLinearLayout.setBackgroundResource(R.drawable.side_nav_bar_zoo);
-        ColorStateList mColorStateList = new ColorStateList(
-                new int[][] {
-                        new int[] {-android.R.attr.state_checked},
-                        new int[] { android.R.attr.state_checked}
-                },
-                new int[] {
-                        ContextCompat.getColor(mActivity, R.color.black),
-                        ContextCompat.getColor(mActivity, R.color.zoo_500),
-                }
-        );
-        mNavigationView.setItemIconTintList(mColorStateList);
-        mNavigationView.setItemTextColor(mColorStateList);
-    }
-
-    static void setZooDarkColors(Activity mActivity) {
-        mActivity.getWindow().setStatusBarColor(ContextCompat.getColor(mActivity, R.color.zoo_200));
-        Objects.requireNonNull(((AppCompatActivity) mActivity).getSupportActionBar())
-                .setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(mActivity, R.color.zoo_500)));
-        NavigationView mNavigationView = mActivity.findViewById(R.id.nav_view);
-        View header = mNavigationView.getHeaderView(0);
-        LinearLayout mLinearLayout = header.findViewById(R.id.nav_header);
-        mLinearLayout.setBackgroundResource(R.drawable.side_nav_bar_zoo);
-        ColorStateList mColorStateList = new ColorStateList(
-                new int[][] {
-                        new int[] {-android.R.attr.state_checked},
-                        new int[] { android.R.attr.state_checked}
-                },
-                new int[] {
-                        ContextCompat.getColor(mActivity, R.color.white),
-                        ContextCompat.getColor(mActivity, R.color.zoo_500),
-                }
-        );
-        mNavigationView.setItemIconTintList(mColorStateList);
-        mNavigationView.setItemTextColor(mColorStateList);
-    }
-
-    static void setStockLightColors(Activity mActivity) {
-        mActivity.getWindow().setStatusBarColor(ContextCompat.getColor(mActivity, R.color.orange_900));
-        Objects.requireNonNull(((AppCompatActivity) mActivity).getSupportActionBar())
-                .setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(mActivity, R.color.yellow_700)));
-        NavigationView mNavigationView = mActivity.findViewById(R.id.nav_view);
-        View header = mNavigationView.getHeaderView(0);
-        LinearLayout mLinearLayout = header.findViewById(R.id.nav_header);
-        mLinearLayout.setBackgroundResource(R.drawable.side_nav_bar);
-        ColorStateList mColorStateList = new ColorStateList(
-                new int[][] {
-                        new int[] {-android.R.attr.state_checked},
-                        new int[] { android.R.attr.state_checked}
-                },
-                new int[] {
-                        ContextCompat.getColor(mActivity, R.color.black),
-                        ContextCompat.getColor(mActivity, R.color.yellow_700),
-                }
-        );
-        mNavigationView.setItemIconTintList(mColorStateList);
-        mNavigationView.setItemTextColor(mColorStateList);
-    }
-
-    static void setStockDarkColors(Activity mActivity) {
-        mActivity.getWindow().setStatusBarColor(ContextCompat.getColor(mActivity, R.color.yellow_700));
-        Objects.requireNonNull(((AppCompatActivity) mActivity).getSupportActionBar())
-                .setBackgroundDrawable(new ColorDrawable(ContextCompat.getColor(mActivity, R.color.yellow_200)));
-        NavigationView mNavigationView = mActivity.findViewById(R.id.nav_view);
-        View header = mNavigationView.getHeaderView(0);
-        LinearLayout mLinearLayout = header.findViewById(R.id.nav_header);
-        mLinearLayout.setBackgroundResource(R.drawable.side_nav_bar);
-        ColorStateList mColorStateList = new ColorStateList(
-                new int[][] {
-                        new int[] {-android.R.attr.state_checked},
-                        new int[] { android.R.attr.state_checked}
-                },
-                new int[] {
-                        ContextCompat.getColor(mActivity, R.color.white),
-                        ContextCompat.getColor(mActivity, R.color.yellow_200)
-                }
-        );
-        mNavigationView.setItemIconTintList(mColorStateList);
-        mNavigationView.setItemTextColor(mColorStateList);
-    }
-
-    static void setZooColors(Activity mActivity) {
-        final String[] darkModeValues = mActivity.getResources().getStringArray(R.array.theme_values);
-        // The apps theme is decided depending upon the saved preferences on app startup
-        String themePref = PreferenceManager.getDefaultSharedPreferences(mActivity)
-                .getString(mActivity.getString(R.string.theme_key), mActivity.getString(R.string.theme_default_value));
-        // Comparing to see which preference is selected and applying those theme settings
-        if (themePref.equals(darkModeValues[0])) {
-            // Check system status and apply colors
-            int nightModeOn = mActivity.getResources().getConfiguration().uiMode &
-                    Configuration.UI_MODE_NIGHT_MASK;
-            switch (nightModeOn) {
-                case Configuration.UI_MODE_NIGHT_YES:
-                    setZooDarkColors(mActivity);
-                    break;
-                case Configuration.UI_MODE_NIGHT_NO:
-                case Configuration.UI_MODE_NIGHT_UNDEFINED:
-                    setZooLightColors(mActivity);
-                    break;
-            }
-        }
-        if (themePref.equals(darkModeValues[1])) {
-            setZooLightColors(mActivity);
-        }
-        if (themePref.equals(darkModeValues[2])) {
-            setZooDarkColors(mActivity);
-        }
-    }
-
-    static void setStockColors(Activity mActivity) {
-        final String[] darkModeValues = mActivity.getResources().getStringArray(R.array.theme_values);
-        // The apps theme is decided depending upon the saved preferences on app startup
-        String themePref = PreferenceManager.getDefaultSharedPreferences(mActivity)
-                .getString(mActivity.getString(R.string.theme_key), mActivity.getString(R.string.theme_default_value));
-        // Comparing to see which preference is selected and applying those theme settings
-        if (themePref.equals(darkModeValues[0])) {
-            // Check system status and apply colors
-            int nightModeOn = mActivity.getResources().getConfiguration().uiMode &
-                    Configuration.UI_MODE_NIGHT_MASK;
-            switch (nightModeOn) {
-                case Configuration.UI_MODE_NIGHT_YES:
-                    setStockDarkColors(mActivity);
-                    break;
-                case Configuration.UI_MODE_NIGHT_NO:
-                case Configuration.UI_MODE_NIGHT_UNDEFINED:
-                    setStockLightColors(mActivity);
-                    break;
-            }
-        }
-        if (themePref.equals(darkModeValues[1])) {
-            setStockLightColors(mActivity);
-        }
-        if (themePref.equals(darkModeValues[2])) {
-            setStockDarkColors(mActivity);
-        }
     }
 }

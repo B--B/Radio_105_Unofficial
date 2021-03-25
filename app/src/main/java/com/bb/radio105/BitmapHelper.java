b8a54f317e544133937710ef43e5ac76b1deea4f
Marco Zanin <mrczn.bb@gmail.com>
2021-03-25 19:37:15 +0100

    WIP: Metadata implementation for notification
    Use Volley for retrieve the data and jSoup for document parse
    Added and updated an helper for album art download and resize from an old google example
    
    NOTE: the bitmap download function is an AsyncTask picked from google examples, but it's not safe.
          The AsyncTask should be static in order to avoid potential memory leakages, but i cannot make
          it static here, or at least I don't know how to do it without workarounds.
          In addition, AsyncTask has been deprecated in API 30
    
    Works, but it's not ready yet
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

---
+++ app/src/main/java/com/bb/radio105/BitmapHelper.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bb.radio105;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class BitmapHelper {
+
+    // Bitmap size for album art in media notification
+    public static final int MEDIA_ART_WIDTH = 256;
+    public static final int MEDIA_ART_HEIGHT = 256;
+
+    public static Bitmap scaleBitmap(int scaleFactor, InputStream is) {
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        // Decode the image file into a Bitmap sized to fill the View
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        return BitmapFactory.decodeStream(is, null, bmOptions);
+    }
+
+    public static int findScaleFactor(int targetW, int targetH, InputStream is) {
+        // Get the dimensions of the bitmap
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeStream(is, null, bmOptions);
+        int actualW = bmOptions.outWidth;
+        int actualH = bmOptions.outHeight;
+        // Determine how much to scale down the image
+        return Math.min(actualW/targetW, actualH/targetH);
+    }
+
+    public static Bitmap fetchAndRescaleBitmap(String uri, int width, int height)
+            throws IOException {
+        URL url = new URL(uri);
+        HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+        httpConnection.setDoInput(true);
+        httpConnection.connect();
+        InputStream inputStream = httpConnection.getInputStream();
+        int scaleFactor = findScaleFactor(width, height, inputStream);
+        httpConnection = (HttpURLConnection) url.openConnection();
+        httpConnection.setDoInput(true);
+        httpConnection.connect();
+        inputStream = httpConnection.getInputStream();
+        return scaleBitmap(scaleFactor, inputStream);
+    }
+}

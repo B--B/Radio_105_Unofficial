737ea32c6f13a8da0caec7365e7ee09668fd1766
Marco Zanin <mrczn.bb@gmail.com>
2021-04-05 14:32:56 +0200

    Notification: set notification channel in manifest
    Instead of creating the notification channel when the foreground service starts, set the channel in android manifest.
    
    NOTE:This is the same way that Google use for their Firebase app
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- app/src/main/java/com/bb/radio105/MusicService.java
+++ app/src/main/java/com/bb/radio105/MusicService.java
@@ -18,8 +18,6 @@
 package com.bb.radio105;
 
 import android.annotation.SuppressLint;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
@@ -122,7 +120,6 @@ public class MusicService extends MediaBrowserServiceCompat implements OnPrepare
 
     // The tag we put on debug messages
     private final static String TAG = "Radio105Player";
-    private static final String CHANNEL_ID = "Radio105ServiceChannel";
 
     // our media player
     static MediaPlayer mPlayer = null;
@@ -575,8 +572,6 @@ public class MusicService extends MediaBrowserServiceCompat implements OnPrepare
         // Get streaming metadata
         getStreamingMetadata();
 
-        // Creating notification channel
-        createNotificationChannel();
         Intent intent = new Intent(this, MainActivity.class);
 
         // Use System.currentTimeMillis() to have a unique ID for the pending intent
@@ -587,7 +582,7 @@ public class MusicService extends MediaBrowserServiceCompat implements OnPrepare
             pIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, PendingIntent.FLAG_UPDATE_CURRENT);
         }
         // Building notification here
-        mNotificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        mNotificationBuilder = new NotificationCompat.Builder(this, getString(R.string.default_notification_channel_id));
         if (pref) {
             mNotificationBuilder.setStyle(new androidx.media.app.NotificationCompat.MediaStyle()
                     .setShowActionsInCompactView(0, 1)
@@ -693,18 +688,6 @@ public class MusicService extends MediaBrowserServiceCompat implements OnPrepare
         //  Browsing not allowed
         if (TextUtils.equals(getString(R.string.app_name), parentId)) {
             result.sendResult(null);
-        }
-    }
-
-    private void createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel serviceChannel = new NotificationChannel(
-                    CHANNEL_ID,
-                    "Foreground Service Channel",
-                    NotificationManager.IMPORTANCE_LOW
-            );
-            NotificationManager manager = getSystemService(NotificationManager.class);
-            manager.createNotificationChannel(serviceChannel);
         }
     }
 

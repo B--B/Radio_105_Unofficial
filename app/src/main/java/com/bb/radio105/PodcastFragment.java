B--B/Radio_105_Unofficial

commit 9e56605169489fd1276376a80a8a0593de87ae8a

[Play Store Policy] WebView: pause audio/video streaming in background
Google does not allow play YouTube videos in background

Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

app/src/main/java/com/bb/radio105/PodcastFragment.java
@@ -224,6 +224,29 @@ public class PodcastFragment extends Fragment {
     }
 
     @Override
+    public void onPause() {
+        if (mWebView != null) {
+            Utils.callJavaScript(mWebView, "player.pause");
+            mWebView.getSettings().setJavaScriptEnabled(false);
+            mWebView.onPause();
+            mWebView.pauseTimers();
+        }
+        super.onPause();
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    public void onResume() {
+        if (mWebView != null) {
+            mWebView.getSettings().setJavaScriptEnabled(true);
+            Utils.callJavaScript(mWebView, "player.play");
+            mWebView.onResume();
+            mWebView.resumeTimers();
+        }
+        super.onResume();
+    }
+
+    @Override
     public void onDestroyView() {
         boolean pref = PreferenceManager.getDefaultSharedPreferences(requireContext())
                 .getBoolean(getString(R.string.screen_on_key), false);

app/src/main/java/com/bb/radio105/ZooFragment.java
@@ -230,6 +230,29 @@ public class ZooFragment extends Fragment {
     }
 
     @Override
+    public void onPause() {
+        if (mWebView != null) {
+            Utils.callJavaScript(mWebView, "player.pause");
+            mWebView.getSettings().setJavaScriptEnabled(false);
+            mWebView.onPause();
+            mWebView.pauseTimers();
+        }
+        super.onPause();
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    public void onResume() {
+        if (mWebView != null) {
+            mWebView.getSettings().setJavaScriptEnabled(true);
+            Utils.callJavaScript(mWebView, "player.play");
+            mWebView.onResume();
+            mWebView.resumeTimers();
+        }
+        super.onResume();
+    }
+
+    @Override
     public void onDestroyView() {
         boolean pref = PreferenceManager.getDefaultSharedPreferences(requireContext())
                 .getBoolean(getString(R.string.screen_on_key), false);

app/src/main/java/com/bb/radio105/Utils.java
@@ -40,6 +40,8 @@ import java.util.Objects;
 
 import static android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN;
 
+import org.adblockplus.libadblockplus.android.webview.AdblockWebView;
+
 public class Utils {
     /**
      * Requests the {@link android.Manifest.permission#WRITE_EXTERNAL_STORAGE} permission.
@@ -132,5 +134,39 @@ public class Utils {
             }
         }
         return line;
+    }
+
+    /**
+     * Call javascript functions in webView.
+     * Credits to mttdat:
+     * https://github.com/mttdat/utils/blob/master/utils/src/main/java/mttdat/utils/WebViewUtils.java
+     *
+     * @param webView    webView object
+     * @param methodName function name
+     * @param params     function parameters
+     */
+    static void callJavaScript(AdblockWebView webView, String methodName, Object...params){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("javascript:try{");
+        stringBuilder.append(methodName);
+        stringBuilder.append("(");
+        for (int i = 0; i < params.length; i++) {
+            Object param = params[i];
+
+            if(param instanceof String){
+                stringBuilder.append("'");
+                stringBuilder.append(param.toString().replace("'", "\\'"));
+                stringBuilder.append("'");
+            }else {
+                stringBuilder.append(param);
+            }
+
+            // If the last param, don't add ','.
+            if(i < params.length - 1){
+                stringBuilder.append(",");
+            }
+        }
+        stringBuilder.append(")}catch(error){Android.onError(error.message);}");
+        webView.loadUrl(stringBuilder.toString());
     }
 }

commit 02986664662809820f368e9305adfa20bb1aedd0

Fix for a bug introduced in 9e56605169489fd1276376a80a8a0593de87ae8a
Can be reproduced 100% of times

1 Open the app and go to Podcast or The 105 Zoo fragment
2 Go to a page with a podcast
3 Exit the app or turn of the screen
4 Open again the app from recents or turn on the screen and unlock the phone
5 Podcast starts even if the user never pressed the play button

Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

app/src/main/java/com/bb/radio105/PodcastFragment.java
@@ -245,7 +245,6 @@ public class PodcastFragment extends Fragment {
     public void onResume() {
         if (mWebView != null) {
             mWebView.getSettings().setJavaScriptEnabled(true);
-            Utils.callJavaScript(mWebView, "player.play");
             mWebView.onResume();
             mWebView.resumeTimers();
         }

app/src/main/java/com/bb/radio105/ZooFragment.java
@@ -251,7 +251,6 @@ public class ZooFragment extends Fragment {
     public void onResume() {
         if (mWebView != null) {
             mWebView.getSettings().setJavaScriptEnabled(true);
-            Utils.callJavaScript(mWebView, "player.play");
             mWebView.onResume();
             mWebView.resumeTimers();
         }
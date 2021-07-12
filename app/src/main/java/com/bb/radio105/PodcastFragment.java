3cd2ad5f960d9b191b76c29543ed05c5b3ec091b
Marco Zanin <mrczn.bb@gmail.com>
2021-07-13 00:36:22 +0200

    PodcastFragment: add back DownloadListener
    Lost somewhere during the code refactor
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- app/src/main/java/com/bb/radio105/PodcastFragment.java
+++ app/src/main/java/com/bb/radio105/PodcastFragment.java
@@ -21,11 +21,13 @@ import static com.bb.radio105.PodcastService.State.Playing;
 import static com.bb.radio105.PodcastService.State.Stopped;
 import static com.bb.radio105.PodcastService.mState;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
@@ -53,6 +55,7 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 import androidx.preference.PreferenceManager;
@@ -355,6 +358,15 @@ public class PodcastFragment extends Fragment implements IPodcastService {
                     mViewCallback.onCustomViewHidden();
                 }
             }
+        });
+
+        mWebView.setDownloadListener((url1, userAgent, contentDisposition, mimetype, contentLength) -> {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+                if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    Utils.requestStoragePermission(requireActivity(), root);
+                }
+            }
+            Utils.startDownload(requireActivity(), url1);
         });
 
         return root;

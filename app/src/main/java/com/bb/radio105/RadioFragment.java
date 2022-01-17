e367c945d71d20339b57f289a2a9895ffd564219
Marco Zanin <mrczn.bb@gmail.com>
2022-01-17 21:33:01 +0100

    Try to fix another illegal state exception on RadioFragment
    I can understand that an old and terribly slow device could trigger this exception... But google console reported this on Android 11.
    I don't know the device/OS combination, but there 's some ROM completely broken out there, because onStart is called after onViewCreated and no matter what happens, the app should be running and visible at that time
    
    Signed-off-by: Marco Zanin <mrczn.bb@gmail.com>

--- app/src/main/java/com/bb/radio105/RadioFragment.java
+++ app/src/main/java/com/bb/radio105/RadioFragment.java
@@ -106,9 +106,9 @@ public class RadioFragment extends Fragment {
 
     @Override
     public void onStart() {
+        super.onStart();
         startMusicService = new Intent(requireContext(), RadioService.class);
         requireContext().startService(startMusicService);
-        super.onStart();
         requireContext().bindService(new Intent(getContext(), RadioService.class), mServiceConnection, 0);
         buildTransportControls();
     }
